40e10f650f89f9cc8402efc69aed059990a3e56f
jmeng-sys <74616703+jmeng-sys@users.noreply.github.com>
2023-04-20 23:24:46 -0600

    Updated searchItem method
    In searchItem method, updated the parameter from "req.query.p" to "req.query.q"

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -47,7 +47,7 @@ const removeOne = async (req, res) => {
 
 const searchItem = async (req, res) => {
   try {
-    const menu = await MenuItems.searchItem(req.query.p);
+    const menu = await MenuItems.searchItem(req.query.q);
     res.send(menu);
   } catch (error) {
     res.status(500).send(error);
